f612149e5e0f43cc81574fb3a90feb97dc8a1f28
ulillilu <ulil___lilu@naver.com>
2020-08-07 21:10:34 +0900

    Updates

---
+++ precache-manifest.55d6e1e9b2fbe0e5fd2f10a5e9a4431b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052a58fcdf5f67d7b8fbb89efde03476",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1757d8828e4fb2121872",
+    "url": "/movie_app_2020/static/css/main.388e1e8e.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1757d8828e4fb2121872",
+    "url": "/movie_app_2020/static/js/main.cfd8804c.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
