8567af3bdef3829e290c6a7792de5fd4fb4e2d8d
ulillilu <ulil___lilu@naver.com>
2020-08-07 23:40:28 +0900

    final

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -14,7 +14,7 @@ class Home extends React.Component {
             data: {
                 data: {movies},
             },
-        } = await axios.get("http://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+        } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
         this.setState({ movies, isLoading: false })
     };
 
